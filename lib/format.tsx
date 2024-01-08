6a25ab3a0f0808583160f647128b965f8240f4ad
Sean Gray <seangray.dev@gmail.com>
2024-01-08 13:17:06 -0500

    remove processCalls function

--- lib/format.tsx
+++ lib/format.tsx
@@ -47,38 +47,3 @@ export const convertDuration = (seconds: number) => {
     secs !== 1 ? 's' : ''
   }`;
 };
-
-export const processCalls = (calls) => {
-  const groupedByDate = {};
-
-  calls.forEach((call) => {
-    const date = new Date(call.created_at).toISOString().split('T')[0];
-    if (!groupedByDate[date]) {
-      groupedByDate[date] = [];
-    }
-    groupedByDate[date].push(call);
-  });
-
-  const groupedAndCounted = Object.entries(groupedByDate).reduce(
-    (acc, [date, calls]) => {
-      const counted = calls.reduce((a, c) => {
-        const key = c.from;
-        if (!a[key]) {
-          a[key] = { ...c, count: 1 };
-        } else {
-          a[key].count++;
-        }
-        return a;
-      }, {});
-
-      acc[date] = Object.values(counted).sort(
-        (a, b) => new Date(b.created_at) - new Date(a.created_at)
-      );
-
-      return acc;
-    },
-    {}
-  );
-
-  return groupedAndCounted;
-};
