cc8317ebfcd9bf24e1c4454c992d143174c04e78
Sean Gray <seangray.dev@gmail.com>
2024-01-08 13:26:17 -0500

    adjust design

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Header from './layout/Header';
 
 export default function Home() {
   return (
-    <main className='flex h-screen flex-col items-center justify-between'>
+    <main className='flex h-screen flex-col items-center justify-between w-[320px] mx-auto'>
       <Header />
       <div className='flex-1 overflow-y-auto'>
-        <ActivityFeed />
+        <ActivityFeed showArchived={false} />
       </div>
       <Footer />
     </main>
